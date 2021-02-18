adbfa9312f455c72873221e185af37d060eed4f8
Kosuke Nagao <kosukengo@gmail.com>
2021-02-18 21:10:33 +0900

    Initial Commit

---
+++ Appdojo_Part06/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Appdojo_Part06
+//
+//  Created by Kosuke Nagao on 2021/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
