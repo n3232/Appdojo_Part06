995e6556e8995c2fbe6fe7c7354cd6c7a5c225d0
Kosuke Nagao <kosukengo@gmail.com>
2021-02-19 09:29:45 +0900

    完成。

--- Appdojo_Part06/ViewController.swift
+++ Appdojo_Part06/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateRandomValue()
     }
 
+    @IBAction func judge(_ sender: Any) {
+        
+        let sliderValue = Int(slider.value)
+        let randomValue = Int(label.text!)
+        
+        // あたり判定
+        var result = "はずれ！"
+        if sliderValue == randomValue {
+            result = "あたり！"
+        }
+        
+        // アラートの表示
+        let alert = UIAlertController(title: "結果", message: "\(result)\nあなたの値：\(sliderValue)", preferredStyle: .alert)
+        let challengeAgainButton = UIAlertAction(title: "再挑戦", style: .default, handler: {action in self.updateRandomValue()})
+        alert.addAction(challengeAgainButton)
+        self.present(alert, animated: true, completion: nil)
+    }
 
+    func updateRandomValue() {
+        let value = arc4random_uniform(99) + 1
+        label.text = String(value)
+    }
+    
 }
 
