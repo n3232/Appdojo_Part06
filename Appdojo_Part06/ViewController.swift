f9428d66430425ee91e6d7b4105e377b546a9bf6
Kosuke Nagao <kosukengo@gmail.com>
2021-02-20 12:04:54 +0900

    swiftlintの警告対応。ランダム値を生成するarc4random_uniformは警告が出たので、Int.randomに変更。

--- Appdojo_Part06/ViewController.swift
+++ Appdojo_Part06/ViewController.swift
@@ -8,37 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     @IBOutlet private weak var label: UILabel!
     @IBOutlet private weak var slider: UISlider!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateRandomValue()
     }
-
-    @IBAction func judge(_ sender: Any) {
-        
+    @IBAction private func judge(_ sender: Any) {
         let sliderValue = Int(slider.value)
         let randomValue = Int(label.text!)
-        
         // あたり判定
         var result = "はずれ！"
         if sliderValue == randomValue {
             result = "あたり！"
         }
-        
         // アラートの表示
-        let alert = UIAlertController(title: "結果", message: "\(result)\nあなたの値：\(sliderValue)", preferredStyle: .alert)
-        let challengeAgainButton = UIAlertAction(title: "再挑戦", style: .default, handler: {action in self.updateRandomValue()})
+        let alert = UIAlertController(
+            title: "結果",
+            message: "\(result)\nあなたの値：\(sliderValue)",
+            preferredStyle: .alert)
+        let challengeAgainButton = UIAlertAction(
+            title: "再挑戦",
+            style: .default,
+            handler: {_ in self.updateRandomValue()})
         alert.addAction(challengeAgainButton)
         self.present(alert, animated: true, completion: nil)
     }
-
-    func updateRandomValue() {
-        let value = arc4random_uniform(99) + 1
+    private func updateRandomValue() {
+        let value = Int.random(in: 1...3)
         label.text = String(value)
     }
-    
 }
-
